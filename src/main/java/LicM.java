42ffa16db12a641a29d8d329c2ae91977fe0bf98
labelle <leelabelle25@gmail,com>
2026-01-28 19:37:16 +0800

    Change main function name to fit coding standard

--- src/main/java/LicM.java
+++ src/main/java/LicM.java
@@ -7,7 +7,7 @@ public class LicM {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        LicMChat();
+        runChat();
     }
 
     private static void showGreeting() {
