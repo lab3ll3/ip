b3c98b22a9d57ad17cf70d484e95dcde047686ce
labelle <leelabelle25@gmail,com>
2026-01-28 19:38:48 +0800

    Added new class "Task"

--- src/main/java/LicM.java
+++ src/main/java/LicM.java
@@ -10,6 +10,33 @@ public class LicM {
         runChat();
     }
 
+    private static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsNotDone() {
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+        @Override
+        public String toString() {
+            return "[" + getStatusIcon() + "] " + description;
+        }
+    }
+
     private static void showGreeting() {
         String logo = """
                    _      _        __  __\s
